61009a1cde1fa9536854f25f693370b243137329
Oscar Rubio <ofrubio0@gmail.com>
2024-06-26 10:53:22 -0500

    (UPDATE) Se aplica el Principio de segregación de la interfaz. La interface IActivities tenia demaciadas cosas. Se dividen en diferentes interfaces para poder asignar a cada rol las actividades que le corresponda

--- 4-InterfaceSegregation/IActivities.cs
+++ 4-InterfaceSegregation/IActivities.cs
@@ -1,11 +1,7 @@
 namespace InterfaceSegregation
 {
-    public interface IActivities
+    public interface IActivities: IWorkTeamActivities, IDesingActivities, IDeveloperctivities, ITesterActivities
     {
-        void Plan();
-        void Comunicate();
-        void Design();
-        void Develop();
-        void Test();
+
     }
 }
---
+++ 4-InterfaceSegregation/IDeveloperActivities.cs
@@ -0,0 +1,7 @@
+namespace InterfaceSegregation
+{
+    public interface IDeveloperctivities
+    {
+        void Develop();
+    }
+}
